d4beebf621ebe8322632c8045f694d0cc2f3e5a9
Shameem Muhammed Shareef <shemim313@gmail.com>
2024-04-23 14:48:34 +0530

    config upd

--- src/components/user/Footer.jsx
+++ src/components/user/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Footer = ()=> {
+    const navigate = useNavigate()
     return (
         <footer className="bg-black dark:bg-gray-900 h-72">
     <div className="container p-6 mx-auto">
@@ -9,11 +11,11 @@ const Footer = ()=> {
                 <div className="grid grid-cols-4 gap-6">
                     <div className="lg:col-span-4">
                         <h3 className="text-gray-700 uppercase dark:text-white">Cities</h3>
-                        <div className="flex mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">
-                            <a href="#" className="ml-8">Calicut</a>
-                            <a href="#" className="ml-16">Ernakulam</a>
-                            <a href="#" className="ml-16">Malappuram</a>
-                            <a href="#" className="ml-16">Kannur</a>
+                        <div className="flex mt-2 text-sm text-gray-600 dark:text-gray-400">
+                            <a href="#" className="ml-8 hover:underline">Calicut</a>
+                            <a href="#" className="ml-16 hover:underline">Ernakulam</a>
+                            <a href="#" className="ml-16 hover:underline">Malappuram</a>
+                            <a href="#" className="ml-16 hover:underline">Kannur</a>
                         </div>
                         <hr className="h-px my-6 bg-gray-200 border-none dark:bg-gray-700" />
                     </div>
@@ -21,16 +23,16 @@ const Footer = ()=> {
                     {/* Blog section */}
                     <div >
                         <h3 className="text-gray-700 uppercase dark:text-white">Helpful Links</h3>
-                        <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Cars</a>
-                        <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Offers</a>
-                        <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">About Us</a>
+                        <a onClick={() => navigate("/cars")} className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Cars</a>
+                        <a href="" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Offers</a>
+                        <a href="" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">About Us</a>
                     </div>
 
                     {/* Contact section */}
                     <div>
                         <h3 className="text-gray-700 uppercase dark:text-white">Contact</h3>
-                        <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">+1 526 654 8965</span>
-                        <span className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">shemim313@gmail.com</span>
+                        <a href="" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">+1 526 654 8965</a>
+                        <a href="" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">shemim313@gmail.com</a>
                     </div>
                 </div>
             </div>
